7b3a6566f9181b0880dc07776f4f49a4e9218e14
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-07-20 21:46:04 +0500

    UPDATED

--- part05.dart
+++ part05.dart
@@ -5,14 +5,17 @@ void main() {
   String name = 'Redjan';
   int age = 20;
 
-  myNumber(favNum, name, age);
+  int output = myNumber(favNum, name, age);
+
+  print(output);
 }
 
-myNumber(int p_favNum, String p_name, int p_age) {
-  int age = p_age;
-  String name = p_name;
+int myNumber(int p_favNum, String p_name, int p_age) {
+  int age = p_age + p_favNum;
+  // String name = p_name;
 
-  stdout.write('My name is $name, I\'am $age years old..');
+  // stdout.write('My name is $name, I\'am $age years old..');
+  return age;
 }
 
 myDisplay() {}
