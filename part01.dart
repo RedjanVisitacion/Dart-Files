d3990b0e386b684bf6a62a5cb1c7533cf7d7413f
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-07-20 19:51:14 +0500

    UPDATED

---
+++ part01.dart
@@ -0,0 +1,14 @@
+void main() {
+  int x;
+  for (x = 0; x < 2; x++) {
+    print("Hello USTP");
+  }
+
+  if (x == 3) {
+    print('The value of x is $x');
+  }else if(x == 2){
+    print('The value of x is equal to 2');
+  } else {
+    print('The value of x is not 3');
+  }
+}
---
+++ part02.dart
@@ -0,0 +1,16 @@
+void main() {
+  String name = 'Redjan';
+  int age = 20;
+  double average = 1.65;
+  bool boy = true;
+  var id = 2023304637;
+  List address = [
+    'P-4 Mobod Oroquieta City', 
+    20, 
+    1.65,
+    true,
+    2023304637 
+    ];
+
+  print(address[3]);
+}
