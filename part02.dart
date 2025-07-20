df18d40af7e9aa7024ef00446d8f3fdaf92c2a3b
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-07-20 19:58:41 +0500

    Updated

--- part02.dart
+++ part02.dart
@@ -4,13 +4,9 @@ void main() {
   double average = 1.65;
   bool boy = true;
   var id = 2023304637;
-  List address = [
-    'P-4 Mobod Oroquieta City', 
-    20, 
-    1.65,
-    true,
-    2023304637 
-    ];
+  List address = ['P-4 Mobod Oroquieta City', 20, 1.65, true, 2023304637];
 
-  print(address[3]);
+  print(
+    'My name is $name, I\'am $age years old. \nMy average grade is $average, \nI\'am boy $boy, \nThen my id is $id, \nLeave in ${address[0]}',
+  );
 }
