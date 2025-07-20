cce24e2c687e92e1543ac1840e29e5db2d19e5c7
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-07-20 21:02:45 +0500

    UPDATED

--- part04.dart
+++ part04.dart
@@ -1,3 +1,19 @@
-void main(){
-  
+void main() {
+  int n1 = 43, n2 = 43;
+
+  // if (n1 > n2) {
+  //   print('First number is greater than second number..');
+  // } else if (n1 == n2) {
+  //   print('First number is equal to second number..');
+  // } else {
+  //   print('Second number is greater then first number..');
+  // }
+
+  String result = n1 > n2
+      ? 'First number is greater than second number..'
+      : n1 == n2
+      ? 'First number is equal to second number..'
+      : 'Second number is greater then first number..';
+
+  print(result);
 }
