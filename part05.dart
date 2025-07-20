c5dc24cc5cfed222b3443b7cc222ec555eae6d0d
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-07-20 21:35:27 +0500

    UPDATED

--- part05.dart
+++ part05.dart
@@ -1,3 +1,18 @@
-void main(){
-  
+import 'dart:io';
+
+void main() {
+  int favNum = 5;
+  String name = 'Redjan';
+  int age = 20;
+
+  myNumber(favNum, name, age);
 }
+
+myNumber(int p_favNum, String p_name, int p_age) {
+  int age = p_age;
+  String name = p_name;
+
+  stdout.write('My name is $name, I\'am $age years old..');
+}
+
+myDisplay() {}
